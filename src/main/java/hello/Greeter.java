61686d829ef1a3640d963f6ad3afbc9e126a0131
GuoJing <x@guojing.me>
2016-12-13 17:32:45 +0800

    init commit

---
+++ src/main/java/hello/Greeter.java
@@ -0,0 +1,24 @@
+package hello;
+
+/**
+ * Created by guojing on 2016/12/13.
+ */
+
+public class Greeter {
+
+    private final long id;
+    private final String content;
+
+    public Greeter(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
---
+++ src/main/java/hello/GreetingController.java
@@ -0,0 +1,20 @@
+package hello;
+
+import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by guojing on 2016/12/13.
+ */
+@RestController
+public class GreetingController {
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @RequestMapping("/greeting")
+    public Greeter greeting(@RequestParam(value="name", defaultValue="World") String name) {
+        return new Greeter(counter.incrementAndGet(), String.format(template, name));
+    }
+}
